30e62d4ad90b44686779e00417256ee5eef56f4c
Gleb Lukicov <Gleb.Lukicov@virginmediao2.co.uk>
2024-02-06 17:06:25 +0000

    .

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -2,10 +2,17 @@ from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
 
+
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    def button_1_click(self, **event_args):
+        user_input = self.text_area_1.text
+        description = anvil.server.call('generate_response', user_input)
+        self.rich_text_2.content = description
 
+
+def text_box_1_pressed_enter(self, **event_args):
+    pass
